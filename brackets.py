30dbe1e5277b9d03f0812903e84cef0e2f9de578
ahvahsky2008 <ahvahsky2008@mail.ru>
2021-09-06 22:55:19 +0300

    Create brackets.py

---
+++ brackets.py
@@ -0,0 +1,15 @@
+def is_balanced(text, brackets="〈〉()[]{}"):
+    opening, closing = brackets[::2], brackets[1::2]
+    stack = [] # keep track of opening brackets types
+    for character in text:
+        if character in opening: # bracket
+            stack.append(opening.index(character))
+        elif character in closing: # bracket
+            if stack and stack[-1] == closing.index(character):
+                stack.pop()  # remove the matched pair
+            else:
+                return False # unbalanced (no corresponding opening bracket) or
+                             # unmatched (different type) closing bracket
+    return (not stack) # no unbalanced brackets
+
+is_balanced("([])")
